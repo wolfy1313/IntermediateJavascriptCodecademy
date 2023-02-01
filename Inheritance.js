ba71a440c3cda0b29d1dc7833848f9c020f34bb0
jeremyHarrell <hillvulf@gmail.com>
2023-01-31 19:19:40 -0600

    inheritance complete

--- Inheritance.js
+++ Inheritance.js
@@ -15,13 +15,47 @@ class HospitalEmployee {
   takeVacationDays(daysOff) {
     this._remainingVacationDays -= daysOff;
   }
+  static generatePassword(){
+    const password = Math.floor(Math.random()*10000);
+    return password
+  }
 }
 
 class Nurse extends HospitalEmployee {
   constructor(name, certifications) {
-    super(name)
+    super(name);
     this._certifications = certifications;
+  } 
+  
+  get certifications() {
+    return this._certifications;
+  }
+  
+  addCertification(newCertification) {
+    this.certifications.push(newCertification);
   }
 }
 
-const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics'])
+class Doctor extends HospitalEmployee {
+  constructor(name, insurance) {
+    super(name);
+    this._insurance = insurance;
+  }
+
+  get insurance(){
+    return this._insurance;
+  }
+  addInsurance(newInsurance){
+    this.insurance.push(newInsurance)
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma','Pediatrics']);
+const doctorWho = new Doctor('Whom', ['Signa', 'Ascension'])
+doctorWho.addInsurance('RobotProof')
+console.log(doctorWho.insurance)
+nurseOlynyk.takeVacationDays(5);
+console.log(nurseOlynyk.remainingVacationDays);
+nurseOlynyk.addCertification('Genetics');
+console.log(nurseOlynyk.certifications);
+console.log(HospitalEmployee.generatePassword());
