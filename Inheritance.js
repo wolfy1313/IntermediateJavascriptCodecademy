9a7e14138cd28c25145c665da7ccebe834666859
jeremyHarrell <hillvulf@gmail.com>
2023-01-31 18:55:25 -0600

    inheritance progress

--- Inheritance.js
+++ Inheritance.js
@@ -3,14 +3,25 @@ class HospitalEmployee {
     this._name = name;
     this._remainingVacationDays = 20;
   }
-
-    get name() {
-      return this._name;
-    }
-    get remainingVacationDays() {
-      return this._remainingVacationDays;
-    } 
-    takeVacationDays(daysOff) {
-      return this._remainingVacationDays -= daysOff
-    }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
 }
+
+class Nurse extends HospitalEmployee {
+  constructor(name, certifications) {
+    super(name)
+    this._certifications = certifications;
+  }
+}
+
+const nurseOlynyk = new Nurse('Olynyk', ['Trauma', 'Pediatrics'])
