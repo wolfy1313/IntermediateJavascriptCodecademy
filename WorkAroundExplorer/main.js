10bbb2868c591cf56bb84fa5ceb1bf315709441a
jeremyHarrell <hillvulf@gmail.com>
2023-02-10 15:03:03 -0600

    continuing on Work Around Project

--- WorkAroundExplorer/main.js
+++ WorkAroundExplorer/main.js
@@ -1,5 +1,5 @@
 // TODO: Add your import statements here.
-import { getRoles, getCompanies } from "./modules/salaryData.js";
+import { getRoles, getCompanies } from "./salaryData.js";
 
 // TODO: Get the companies and roles using the salaryData module.
 const companies = getCompanies();
--- WorkAroundExplorer/workAroundModule.js
+++ WorkAroundExplorer/workAroundModule.js
@@ -1,6 +1,6 @@
 // Add your imports here.
-import { getDataByRole, getDataByCompany } from "./modules/salaryData.js"
-import salaryData from "./modules/salaryData.js"
+import { getDataByRole, getDataByCompany } from "./salaryData.js"
+import salaryData from "./salaryData.js"
 
 // Replace the empty array with the appropriate imported function/value
 const getAverageSalaryByRole = role => {
